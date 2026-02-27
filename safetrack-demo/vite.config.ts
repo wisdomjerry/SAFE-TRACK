03daa3b04b157328c586d83f8a27df793158d0fe
wisdomjerry <wisdom.jeremiah.upti@gmail.com>
2026-02-27 10:41:32 +0300

    remove error

--- safetrack-demo/vite.config.ts
+++ safetrack-demo/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // This forces Vite to find the correct entry point for Mapbox
-      'react-map-gl': 'react-map-gl/mapbox'
-    }
+  optimizeDeps: {
+    include: ['react-map-gl', 'mapbox-gl']
   }
 })
